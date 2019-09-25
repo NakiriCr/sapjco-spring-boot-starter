709cfcc9eaaecfd04cbf857541cb21b9b6cbe371
VirtualCry <172519267@QQ.COM>
2019-09-25 11:33:22 +0800

    update demo.

--- sapjco-spring-boot-starter-sample/src/main/java/cn/gitlab/virtualcry/sapjco/spring/boot/demo/endpoint/ClientEndpoint.java
+++ sapjco-spring-boot-starter-sample/src/main/java/cn/gitlab/virtualcry/sapjco/spring/boot/demo/endpoint/ClientEndpoint.java
@@ -1,13 +1,20 @@
 package cn.gitlab.virtualcry.sapjco.spring.boot.demo.endpoint;
 
 import cn.gitlab.virtualcry.sapjco.beans.factory.JCoConnectionFactory;
+import cn.gitlab.virtualcry.sapjco.client.JCoClient;
+import cn.gitlab.virtualcry.sapjco.client.handler.FunctionRequestHandler;
 import cn.gitlab.virtualcry.sapjco.config.JCoSettings;
+import cn.gitlab.virtualcry.sapjco.spring.boot.demo.vo.function.zmm_shp_getdnhb.DnHeader;
+import cn.gitlab.virtualcry.sapjco.spring.boot.demo.vo.function.zmm_shp_getdnhb.TableParameter;
 import cn.gitlab.virtualcry.sapjco.util.data.JCoDataUtils;
-import lombok.Data;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.HashMap;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
 
@@ -23,46 +30,42 @@ public class ClientEndpoint {
     @Resource
     private JCoConnectionFactory        connectionFactory;
 
+    @GetMapping("names")
+    public Collection<String> getAllClientNames() {
+        return connectionFactory.getClients().keySet();
+    }
 
-    @PostMapping("{clientName}/create")
-    public void createClient(@PathVariable String clientName, @RequestBody JCoSettings settings) {
-        this.connectionFactory.createClient(clientName, settings);
+    @GetMapping("invoke")
+    public Map<String, Object> invokeSapFunc() {
+        JCoClient client = connectionFactory.getClient("main-client");
+        String functionName = "ZMM_SHP_GETDNHB";
+        FunctionRequestHandler requestHandler = (importParameter, tableParameter, changingParameter) -> {
+            TableParameter tableParameterValue = TableParameter.builder()
+                    .dnHeaders(Collections.singletonList(DnHeader.builder().dnNo("0080055489").build()))
+                    .build();
+            JCoDataUtils.setJCoParameterListValue(tableParameter, tableParameterValue);
+        };
+        return client.invokeSapFunc(functionName, requestHandler);
+    }
+
+    @GetMapping("invokeToBean")
+    public TableParameter invokeSapFuncToBean() {
+        JCoClient client = connectionFactory.getClient("main-client");
+        String functionName = "ZMM_SHP_GETDNHB";
+        FunctionRequestHandler requestHandler = (importParameter, tableParameter, changingParameter) -> {
+            TableParameter tableParameterValue = TableParameter.builder()
+                    .dnHeaders(Collections.singletonList(DnHeader.builder().dnNo("0080055489").build()))
+                    .build();
+            JCoDataUtils.setJCoParameterListValue(tableParameter, tableParameterValue);
+        };
+        return client.invokeSapFunc(functionName, requestHandler, TableParameter.class);
     }
 
     @GetMapping("{clientName}/settings")
-    public JCoSettings getClientSettings(@PathVariable String clientName) {
+    public JCoSettings getServerSettings(@PathVariable String clientName) {
         return Optional
                 .ofNullable(this.connectionFactory.getClient(clientName))
                 .orElseThrow(() -> new RuntimeException("Could not find client: [" + clientName + "]"))
                 .getSettings();
-    }
-
-    @PostMapping("{clientName}/invoke/{functionName}")
-    public Map<String, Object> invokeSapFunc(@PathVariable String clientName,
-                                             @PathVariable String functionName,
-                                             @RequestBody RequestParameter requestParameter) {
-        Map<String, Object> invokeResult = new HashMap<>();
-        Optional.ofNullable(this.connectionFactory.getClient(clientName))
-                .orElseThrow(() -> new RuntimeException("Could not find client: [" + clientName + "]"))
-                .invokeSapFunc(
-                        functionName,
-                        (importParameter, tableParameter, changingParameter) -> {
-                            if (requestParameter.getImportParameter() != null)
-                                JCoDataUtils.setJCoParameterListValue(importParameter, requestParameter.getImportParameter());
-                            if (requestParameter.getImportParameter() != null)
-                                JCoDataUtils.setJCoParameterListValue(tableParameter, requestParameter.getTableParameter());
-                            if (requestParameter.getImportParameter() != null)
-                                JCoDataUtils.setJCoParameterListValue(changingParameter, requestParameter.getChangingParameter());
-                        },
-                        response -> response.forEach(jCoField -> invokeResult.put(jCoField.getName(), JCoDataUtils.getJCoFieldValue(jCoField)))
-                );
-        return invokeResult;
-    }
-
-    @Data
-    private static class RequestParameter {
-        private Map<String, Object> importParameter;
-        private Map<String, Object> tableParameter;
-        private Map<String, Object> changingParameter;
     }
 }
